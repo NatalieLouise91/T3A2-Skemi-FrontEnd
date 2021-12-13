c480179a4b40ede81a0b99fd5c346139a07dcb40
NatalieLouise91 <natalie936@hotmail.com>
2021-12-13 10:39:08 +1000

    starting setting up responsive menu bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,37 +1,72 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+
+import {
+    AppBar,
+    Toolbar,
+    CssBaseline,
+    Typography,
+    makeStyles,
+    useTheme,
+} from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+    navlinks: {
+        marginLeft: theme.spacing(5),
+        display: "flex",
+    },
+    logo: {
+        flexGrow: "1",
+        cursor: "pointer",
+    },
+    link: {
+        textDecoration: "none",
+        color: "white",
+        fontSize: "20px",
+        marginLeft: theme.spacing(20),
+        "&:hover": {
+            color: "yellow",
+            borderBottom: "1px solid white",
+        },
+    },
+}));
 
 const Nav = ({loggedInUser, setLoggedInUser}) => {
+
     function logout(event) {
         event.preventDefault();
         setLoggedInUser("");
     }
+    
+    const classes = useStyles();
 
     return (
-        <AppBar>
+        <AppBar position="static">
+            <CssBaseline />
             <Toolbar>
-                <Typography>Skemi</Typography>
-            <ul>
-                <Typography><li><Link to="/">Home</Link></li></Typography>
-            </ul>
-                {loggedInUser?
-            <ul>
-                <Typography><li>{loggedInUser.first_name}</li></Typography>
-                <Typography><li><Link to="/create-event">Create an Event</Link></li></Typography>
-                <Typography><li><Link to="/event-schedule">Event Schedule</Link></li></Typography>
-                <Typography><li><Link to="/home" onClick ={logout}>Logout</Link></li></Typography>
-            </ul>
-                :
+                <Typography variant="h4" className={classes.logo}>
+                    Skemi
+                </Typography>
 
-            <ul>
-                <Typography><li><Link to="/login">Login</Link></li></Typography>
-                <Typography><li><Link to="/new-user">Register</Link></li></Typography>
-            </ul>
+                <>
+                <Typography><Link to="/">Home</Link></Typography>
+                </>
+                    {loggedInUser?
+                <>
+                    <Typography>{loggedInUser.first_name}</Typography>
+                    <Typography><Link to="/create-event">Create an Event</Link></Typography>
+                    <Typography><Link to="/event-schedule">Event Schedule</Link></Typography>
+                    <Typography><Link to="/home" onClick ={logout}>Logout</Link></Typography>
+                </>
+                    :
 
-                }
+                <>
+                    <Typography><Link to="/login">Login</Link></Typography>
+                    <Typography><Link to="/new-user">Register</Link></Typography>
+                </>
+                    }
+
             </Toolbar>
         </AppBar>
     )
