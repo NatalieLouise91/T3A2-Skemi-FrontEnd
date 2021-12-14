12c62c8e97ab4db77f85ef4003dad8cffa9abaf5
Dung Nguyen <nddung2812@gmail.com>
2021-12-14 15:29:14 +1000

    remove comments in authServices.js

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -3,18 +3,10 @@ import skemiAPI from "../config/api.js"
 export async function signUp(data) {
     const response = await skemiAPI.post(`api/auth/sign_up`,data)
     return response.data
-    // return {
-    //     username: "Test",
-    //     jwt: "token"
-    // }
 }
 
 export async function signIn(data) {
     const response = await skemiAPI.post(`api/auth/sign_in`,data)
     return response.data
-    // return {
-    //     username: "Test",
-    //     jwt: "token"
-    // }
 }
 
