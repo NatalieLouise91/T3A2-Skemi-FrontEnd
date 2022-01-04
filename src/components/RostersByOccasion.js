b77ac04e2bc0a6a1ed1373a9e1758e016247e174
NatalieLouise91 <natalie936@hotmail.com>
2022-01-04 13:37:02 +1000

    Added Roster Card, enabled delete functionality on View Roster Component

--- src/components/RostersByOccasion.js
+++ src/components/RostersByOccasion.js
@@ -1,39 +1,30 @@
-import React, { useState, useEffect } from "react";
-import { Link, useParams, useNavigate } from "react-router-dom";
+import React from "react";
+import { useParams } from "react-router-dom";
 import { useGlobalState } from "../utils/stateContext";
 import {
    Grid,
+   Container,
 } from "@material-ui/core";
-// import { getRosterById, deleteRoster } from "../services/rosterServices";
+import RosterCard from "./RosterCard";
 
 export default function RostersByOccasion() {
 
     const { id } = useParams();
-    // const navigate = useNavigate();
-    // const [roster, setRoster] = useState();
     const { store } = useGlobalState();
     const { rosters } = store; 
 
-    // useEffect(() => {
-    //     getRosterById(id)
-    //        .then((roster) => setRoster(roster))
-    //        .catch((error) => console.log(error));
-    //  }, [id]);
-  
-    //  if (!roster) return null;
-    //  const removeRoster = () => {
-    //     deleteRoster(id)
-    //        .then(navigate("/"))
-    //        .catch((error) => console.log(error));
-    //  };
-
     return (
-        <Grid container spacing={2}>
-            {rosters.map((roster) =>
-                roster.event_id == id?
-                <Grid item xs={8} key={roster.id}>{roster.name}, {roster.start_time} - {roster.end_time}</Grid> : null
-            )
-            }
-        </Grid>
+        <Container>
+            <Grid container spacing={3}>
+                    {rosters.map((roster) =>
+                    roster.event_id == id?
+                            <Grid key={roster.id} item xs={12} sm={6} md={3}>
+                                <RosterCard roster={roster} />
+                            </Grid>
+                    : null
+                )
+                }
+            </Grid>
+        </Container>
     )
 }
