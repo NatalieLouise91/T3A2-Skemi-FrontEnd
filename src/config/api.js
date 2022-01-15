6b5b9ad62e4f6a467751ec689eb72b1b57810f68
Jordan <jordan.s.hardy@gmail.com>
2022-01-15 23:41:10 +1000

    comments

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+//variable to set axios api url
 const skemiAPI = axios.create({
    baseURL: "http://localhost:3000",
 });
 
+// getting and setting auth token
 skemiAPI.interceptors.request.use((req) => {
    const token = sessionStorage.getItem("token");
    console.log("Set token header: ", token);
